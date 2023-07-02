f24695727499425f9778042bf9c6fda585ba8503
energoagent <kovalevvb@hotmail.com>
2023-07-02 19:17:43 +0300

    complete wok with google drive - finally

--- gdiskstorage/urls.py
+++ gdiskstorage/urls.py
@@ -6,11 +6,10 @@ appname = 'gdiskstorage'
 
 urlpatterns = [
     path('', views.GDiskFolderListView.as_view(), name = 'list'),
-#    path('', views.gdisk_list, name = 'list'),
     path('detail/', views.gdiskfolder_detail, name = 'detail'),
     path('detail/downloadimage', views.gdiskfolder_downloadimage, name = 'downloadimage'),
-#    path('detail/loadimage', views.albumloadimage, name = 'loadimage'),
-#    path('detail/deleteimage', views.albumdeleteimage, name = 'deleteimage'),
+    path('detail/uploadimage', views.gdiskfolder_uploadimage, name = 'uploadimage'),
+    path('detail/deleteimage', views.gdiskfolder_deleteimage, name = 'deleteimage'),
     path('create/', views.gdiskfolder_create, name = 'create'),
     path('update/', views.gdiskfolder_update, name = 'update'),
     path('delete/', views.gdiskfolder_delete, name = 'delete'),
--- gdiskstorage/views.py
+++ gdiskstorage/views.py
@@ -12,7 +12,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.http import MediaIoBaseDownload, MediaInMemoryUpload
 
 # other modules
 from random import choice
@@ -25,19 +25,19 @@ from gdiskstorage.models import GDiskFolder, GDISK_PATH
 from esadb.settings import BASE_DIR
 
 # If modifying these scopes, delete the file token.json.
-#SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata',
-          'https://www.googleapis.com/auth/drive',
-          'https://www.googleapis.com/auth/drive.file'
-          ]
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file',
+    'https://www.googleapis.com/auth/drive.appdata',
+    ]
 
 # Google drive service open and return
 def gdrive_open():
     status = None
     creds = None
     home_dir = os.path.expanduser('~')
-    credential_path = os.path.join(home_dir,'client_secret_esadb.json')
-    token_path = os.path.join(home_dir,'token_esadb.json')
+    credential_path = os.path.join(home_dir,'esadb_client_secret.json')
+    token_path = os.path.join(home_dir,'esadb_token.json')
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
@@ -58,18 +58,6 @@ def gdrive_open():
     except HttpError as status: pass
     return {'service': service, 'status': status}
 
-@require_http_methods(['GET'])
-def gdisk_list(request):
-    srv = gdrive_open()
-    if srv['status'] == None:
-        # Call the Drive v3 API
-        results = srv['service'].files().list(fields="files(id, name)").execute()
-        items = results.get('files', [])
-        context = {'filelist': items}
-    else:
-        context = {'status': srv['status']}
-    return render(request, 'gdisk_list.html', context = context)
-
 class GDiskFolderListView(CompleteListView):
     model = GDiskFolder
     paginate_by = 10
@@ -77,7 +65,7 @@ class GDiskFolderListView(CompleteListView):
     template_name = 'gdiskfolder_list.html'
     subtitle = 'Медиа: альбомы изображений'
     contextmenu = {
-        'Изображения': 'formmethod=GET formaction=detail/',
+        'Просмотреть': 'formmethod=GET formaction=detail/',
         'Изменить': 'formmethod=GET formaction=update/',
         'Добавить': 'formmethod=GET formaction=create/',
         'Удалить': 'formmethod=GET formaction=delete/',
@@ -116,16 +104,17 @@ def gdiskfolder_create(request):
             if ownerclassname != None:
                 owner = eval(ownerclassname + '.objects.get(id = ownerid)')
                 if owner != None: owner.albums.add(folder)
-#            remotedir = os.path.join(GDISK_PATH, folder.folder)
 # google drive
             srv = gdrive_open()
             if srv['status'] == None:
-# Call the Drive v3 API
-                folder_metadata = {'name': folder.folder, 'parents': [],
+# get id of google storage
+                results = srv['service'].files().list(q = 'name = "gdiskstorage"', fields = 'files(id, name)').execute()
+                for item in results.get('files', []): 
+                    parent_id = item['id']
+                folder_metadata = {'name': folder.folder, 'parents': [parent_id],
                     'mimeType': 'application/vnd.google-apps.folder'}
                 create_folder = srv['service'].files().create(body=folder_metadata, fields='id').execute()
-                folder_id = create_folder.get('id', [])
-                print('FLD_ID:', folder_id)
+                folderid = create_folder.get('id', [])
             return redirect('../')
     else: 
         folderform = GDiskFolderModelForm()
@@ -139,19 +128,19 @@ def gdiskfolder_create(request):
 @require_http_methods(['GET', 'POST'])
 def gdiskfolder_update(request):
     if request.method == 'POST':
-        folderid = request.session.get('albumid')
+        folderid = request.session.get('folderid')
         if folderid != None: 
             folderform = GDiskFolderModelForm(request.POST, instance = GDiskFolder.objects.get(id = folderid))
             if folderform.is_valid():
                 folder = folderform.save()
         return redirect('../')
     else: 
-        folderid = request.GET.get('albumid')
+        folderid = request.GET.get('folderid')
         if folderid != None: 
             folderform = GDiskFolderModelForm(instance = GDiskFolder.objects.get(id = folderid))
         else:
             return redirect('../')
-        request.session['albumid'] = folderid
+        request.session['folderid'] = folderid
         request.session.modified = True
     context = {'form': folderform,
         'contextmenu': {'Отменить':'formmethod=GET formaction=../', 'Подтвердить':'formmethod=POST'},
@@ -161,39 +150,74 @@ def gdiskfolder_update(request):
 @require_http_methods(['GET', 'POST'])
 def gdiskfolder_delete(request):
     if request.method == 'GET':
-        folderid = request.GET.get('albumid')
-        if folderid == None: folderid = request.session.get('albumid')
+        folderid = request.GET.get('folderid')
+        if folderid == None: folderid = request.session.get('folderid')
         if folderid != None:
             context = {'folder': GDiskFolder.objects.get(id = folderid),
                 'contextmenu':{'Отменить':'formmethod=GET formaction=../', 'Удалить': 'formmethod=POST'}, 
                 'subtitle':'Медиа: альбомы изображений: удаление'}
             return render(request, 'gdiskfolder_confirm_delete.html', context = context)
     else: 
-        folderid = request.POST.get('albumid')
+        folderid = request.POST.get('folderid')
+        if folderid == None: folderid = request.session.get('folderid')
         if folderid != None:
-            GDiskFolder.objects.filter(id = folderid).delete()
+            folder = GDiskFolder.objects.get(id = folderid)
+# google drive
+            srv = gdrive_open()
+            if srv['status'] == None:
+                try:
+# get folder id
+                    results = srv['service'].files().list(q = ('name = %r and \
+                        mimeType = "application/vnd.google-apps.folder"' %folder.folder), 
+                        fields = 'files(id, name)').execute()
+                    for item in results.get('files', []): 
+                        parent_id = item['id']
+# delete files
+#                    results = srv['service'].files().list(q = ('%r in parents and \
+#                        mimeType!="application/vnd.google-apps.folder" and \
+#                        trashed != True' %parent_id), fields = 'files(id)').execute()
+#                    for item in results.get('files', []):
+#                        srv['service'].files().delete(fileId = item['id']).execute()
+# delete folder
+                    srv['service'].files().delete(fileId = parent_id).execute()
+                except BaseException as e: 
+                    print('EXEPTION:', e)
+                folder.delete()
     return redirect('../')
 
 @require_http_methods(['GET'])
 def gdiskfolder_detail(request):
-    folderid = request.GET.get('albumid')
+    folderid = request.GET.get('folderid')
     if folderid == None: 
-        folderid = request.session.get('albumid')
+        folderid = request.session.get('folderid')
     if folderid == None: 
         return redirect('../')
     folder = GDiskFolder.objects.get(id = folderid)
-    request.session['albumid'] = folder.pk
+    request.session['folderid'] = folder.pk
     request.session.modified = True
     srv = gdrive_open()
     if srv['status'] == None:
-#        info = srv['service'].about().get()
-#        print('-->', type(info))
-        # Call the Drive v3 API
-        results = srv['service'].files().list(fields="files(id, name)").execute()
+        about = srv['service'].about().get(fields = 'user').execute()
+# get id of google storage
+#        results = srv['service'].files().list(q = 'name = "gdiskstorage"', fields = 'files(id, name)').execute()
+#        for item in results.get('files', []): 
+#            parent_id = item['id']
+# get id of storage folder
+        results = srv['service'].files().list(q = ('name = %r and \
+            mimeType = "application/vnd.google-apps.folder"' %folder.folder), 
+            fields = 'files(id, name, webViewLink)').execute()
+        for item in results.get('files', []): 
+            parent_id = item['id']
+            folder_link = item['webViewLink']
+# get files in folder
+        results = srv['service'].files().list(q = ('%r in parents and \
+            mimeType!="application/vnd.google-apps.folder" and \
+            trashed != True' %parent_id), fields = 'files(id, name, webViewLink)').execute()
         items = results.get('files', [])
-    context = {'status': srv['status'], 'folder': folder, 'filelist': items,
+#----------------------------------------------------------
+    context = {'status': srv['status'], 'folder': folder, 'filelist': items, 'about': about, 'folder_link': folder_link, 
         'contextmenu':{'Скачать': 'formmethod=GET formaction=downloadimage',
-            'Загрузить': 'formmethod=GET formaction=loadimage',
+            'Загрузить': 'formmethod=GET formaction=uploadimage',
             'Удалить': 'formmethod=GET formaction=deleteimage',
             'Вернуться': 'formmethod=GET formaction=../'},
         'subtitle':'Медиа: альбомы изображений: просмотр'}
@@ -214,4 +238,43 @@ def gdiskfolder_downloadimage(request):
                     status, done = downloader.next_chunk()
     return redirect('../')
 
+@require_http_methods(['GET', 'POST'])
+def gdiskfolder_uploadimage(request):
+    folderid = request.session.get('folderid')
+    if folderid == None:
+        return redirect('../')
+    if request.method == 'POST':
+        if request.FILES == None:
+            return redirect('../')
+        folder = GDiskFolder.objects.get(id = folderid)
+        srv = gdrive_open()
+        if srv['status'] == None:
+            try:
+# get id of storage folder
+                results = srv['service'].files().list(q = ('name = %r and \
+                    mimeType = "application/vnd.google-apps.folder"' %folder.folder), 
+                    fields = 'files(id, name)').execute()
+                for item in results.get('files', []): 
+                    folder_id = item['id']
+# get files in folder
+                for fl in request.FILES.getlist('filelist'):
+                    media = MediaInMemoryUpload(fl.read())
+                    srv['service'].files().create(body = {'name': fl.name, 'parents': [folder_id]}, media_body = media).execute()
+            except BaseException as e: 
+                print('EXEPTION:', e)
+        return redirect('../')
+    else:
+        context = {'status':'',
+            'contextmenu':{'Подтвердить': 'formmethod=POST', 'Вернуться': 'formmethod=GET formaction=../'},
+            'subtitle':'Медиа: альбомы изображений: загрузка'}
+        return render(request, 'gdiskfolder_loadimages.html', context = context)
+
+@require_http_methods(['GET'])
+def gdiskfolder_deleteimage(request):
+    srv = gdrive_open()
+    if srv['status'] == None:
+        for item in request.GET.getlist('imgid'):
+            file_id, file_name = item.split(':')
+            srv['service'].files().delete(fileId = file_id).execute()
+    return redirect('../')
 
